f2d10a26fc801a74fb530a224742a0b280e83856
Sean Altemose <sean@seanaltemose.com>
2019-04-18 20:19:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,8 @@ function basicTeenager(age) {
 function teenager(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!"
-  } else return "You are not a teenager" }
+  } else if (age < 13 || age > 19) {
+    return "You are not a teenager" }
 
 }
 
