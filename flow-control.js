42cf44adfdb9d484e77622018ba4a09799feccf0
Sean Altemose <sean@seanaltemose.com>
2019-04-18 20:21:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,5 +29,8 @@ function ternaryTeenager(age){
 }
 
 function switchAge(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else if (age < 13 || age > 19) {
+    return "You have an age" }
 }
